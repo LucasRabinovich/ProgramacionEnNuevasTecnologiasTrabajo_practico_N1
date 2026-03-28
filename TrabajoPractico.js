1d6584930dfc462f5dca000619734035290c5569
lucas.mrabinovich@gmail.com <lucas.mrabinovich@gmail.com>
2026-03-28 17:39:03 -0300

    Resolucion parte 2

--- TrabajoPractico.js
+++ TrabajoPractico.js
@@ -1,9 +1,9 @@
 const productos = [
-    { id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },
-    { id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },
-    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" },
-    { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
-    { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
+    { id: 1, nombre: "Remera", precio: 10000, categoria: "Ropa" },
+    { id: 2, nombre: "Pantalón", precio: 30000, categoria: "Ropa" },
+    { id: 3, nombre: "Zapatillas", precio: 100000, categoria: "Calzado" },
+    { id: 4, nombre: "Gorra", precio: 20000, categoria: "Accesorios" },
+    { id: 5, nombre: "Campera", precio: 25000, categoria: "Ropa" }
 ];
 
 console.log(productos[0].nombre);
@@ -15,3 +15,14 @@ for (const producto of productos) {
 productos.forEach(producto => {
     console.log(`Producto: ${producto.nombre} - Precio: $${producto.precio}`);
 });
+const nombresProductos = productos.map(producto => producto.nombre);
+console.log(nombresProductos);
+
+const ropa = productos.filter(producto => producto.categoria === "Ropa");
+console.log(ropa);
+
+const productosCaros = productos.filter(producto => producto.precio > 3000);
+console.log(productosCaros);
+
+const gorra = productos.find(producto => producto.nombre === "Gorra");
+console.log(gorra);
